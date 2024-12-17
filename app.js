22f23480ac7ad4dc66e7063190e4076579e3269f
BabaVo55 <ethemcode@outlook.com>
2024-12-17 15:51:33 +0000

    finalising

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const particles = [];
 const numParticles = 100;
@@ -17,77 +16,213 @@ const images = [
     "imgs/killua.jpg",
     "imgs/naruto.jpg",
     "imgs/saitama.jpg",
-    "imgs/zoro.jpg"
+    "imgs/zoro.jpg",
+    
 ];
 
 class Particle {
-    constructor(size, x, y, speedX, speedY, image){
+    constructor(size, x, y, speedX, speedY, image) {
         this.size = size;
         this.x = x;
         this.y = y;
         this.speedX = speedX * SPEED_MULTIPLIER;
         this.speedY = speedY * SPEED_MULTIPLIER;
         this.image = new Image();
-        this.image.src = image
+        this.image.src = image;
+
+        this.image.onload = () => {
+            this.loaded = true;
+        };
+        this.loaded = false;
     }
 
-    update(){
+    update() {
         this.x += this.speedX;
         this.y += this.speedY;
 
-        if (this.x + this.size > canvas.width || this.x < 0){
+        if (this.x + this.size > canvas.width || this.x < 0) {
             this.speedX = -this.speedX;
         }
-        if (this.y + this.size > canvas.height || this.speedY < 0){
+        if (this.y + this.size > canvas.height || this.y < 0) {
             this.speedY = -this.speedY;
         }
 
-        if (mouseX !== null && mouseY !== null){
-            const dist = Math.hypot(mouseX - this.x, mouseY - this.y)
+        if (mouseX !== null && mouseY !== null) {
+            const dist = Math.hypot(mouseX - this.x, mouseY - this.y);
 
             if (dist < 150) {
                 const angle = Math.atan2(this.y - mouseY, this.x - mouseX);
                 this.x += Math.cos(angle) * 2;
                 this.y += Math.sin(angle) * 2;
 
-                if (this.x < 0) 
-                    this.x = 0;
-                if (this.x + this.size > canvas.width) 
-                    this.x = canvas.width - this.size;
-                if (this.y < 0) 
-                    this.y = 0;
-                if (this.y + this.size > canvas.height) 
-                    this.y = canvas.height - this.size
+                if (this.x < 0) this.x = 0;
+                if (this.x + this.size > canvas.width) this.x = canvas.width - this.size;
+                if (this.y < 0) this.y = 0;
+                if (this.y + this.size > canvas.height) this.y = canvas.height - this.size;
             }
         }
     }
 
     draw() {
-        ctx.drawImage(this.image, this.x, this.y, this.size, this.size);
+        if (this.loaded) {
+            ctx.drawImage(this.image, this.x, this.y, this.size, this.size);
+        }
     }
 }
 
-function init(){
-    for (let i = 0; i < numParticles; i++){
-        const size = 40 * Math.random() * 20;
+function init() {
+    particles.length = 0; // Clear existing particles
+    for (let i = 0; i < numParticles; i++) {
+        const size = 40 + Math.random() * 20;
         const x = Math.random() * (canvas.width - size);
         const y = Math.random() * (canvas.height - size);
         const speedX = Math.random() - 0.5;
         const speedY = Math.random() - 0.5;
-        const image = images[Math.floor(Math.random() * image.length)];
-        particles.push(new Particle(size, x, y, speedX, speedY, image))
+        const image = images[Math.floor(Math.random() * images.length)];
+        particles.push(new Particle(size, x, y, speedX, speedY, image));
     }
 }
 
-function animate(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    numParticles.forEach((particle) => {
-        particle.update()
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach((particle) => {
+        particle.update();
         particle.draw();
-
-    })
-    requestAnimationFrame(animate)
+    });
+    requestAnimationFrame(animate);
 }
 
-init()
-animate()
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init(); // Reinitialize particles on resize
+});
+
+window.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+window.addEventListener('mouseleave', () => {
+    mouseX = null;
+    mouseY = null;
+});
+
+init();
+animate();
+
+
+
+
+
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// const particles = [];
+// const numParticles = 100;
+// const SPEED_MULTIPLIER = 0.5;
+// let mouseX = null;
+// let mouseY = null;
+
+// const images = [
+//     "imgs/gojo.jpg",
+//     "imgs/haikyuu.jpg",
+//     "imgs/kakashi.jpg",
+//     "imgs/killua.jpg",
+//     "imgs/naruto.jpg",
+//     "imgs/saitama.jpg",
+//     "imgs/zoro.jpg"
+// ];
+
+// class Particle {
+//     constructor(size, x, y, speedX, speedY, image) {
+//         this.size = size;
+//         this.x = x;
+//         this.y = y;
+//         this.speedX = speedX * SPEED_MULTIPLIER;
+//         this.speedY = speedY * SPEED_MULTIPLIER;
+//         this.image = new Image();
+//         this.image.src = image;
+
+//         this.image.onload = () => {
+//             this.loaded = true;
+//         };
+//         this.loaded = false;
+//     }
+
+//     update() {
+//         this.x += this.speedX;
+//         this.y += this.speedY;
+
+//         if (this.x + this.size > canvas.width || this.x < 0) {
+//             this.speedX = -this.speedX;
+//         }
+//         if (this.y + this.size > canvas.height || this.y < 0) {
+//             this.speedY = -this.speedY;
+//         }
+
+//         if (mouseX !== null && mouseY !== null) {
+//             const dist = Math.hypot(mouseX - this.x, mouseY - this.y);
+
+//             if (dist < 150) {
+//                 const angle = Math.atan2(this.y - mouseY, this.x - mouseX);
+//                 this.x += Math.cos(angle) * 2;
+//                 this.y += Math.sin(angle) * 2;
+
+//                 if (this.x < 0) this.x = 0;
+//                 if (this.x + this.size > canvas.width) this.x = canvas.width - this.size;
+//                 if (this.y < 0) this.y = 0;
+//                 if (this.y + this.size > canvas.height) this.y = canvas.height - this.size;
+//             }
+//         }
+//     }
+
+//     draw() {
+//         if (this.loaded) {
+//             ctx.drawImage(this.image, this.x, this.y, this.size, this.size);
+//         }
+//     }
+// }
+
+// function init() {
+//     particles.length = 0; // Clear existing particles
+//     for (let i = 0; i < numParticles; i++) {
+//         const size = 40 + Math.random() * 20;
+//         const x = Math.random() * (canvas.width - size);
+//         const y = Math.random() * (canvas.height - size);
+//         const speedX = Math.random() - 0.5;
+//         const speedY = Math.random() - 0.5;
+//         const image = images[Math.floor(Math.random() * images.length)];
+//         particles.push(new Particle(size, x, y, speedX, speedY, image));
+//     }
+// }
+
+// function animate() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     particles.forEach((particle) => {
+//         particle.update();
+//         particle.draw();
+//     });
+//     requestAnimationFrame(animate);
+// }
+
+// window.addEventListener('resize', () => {
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
+//     init(); // Reinitialize particles on resize
+// });
+
+// window.addEventListener('mousemove', (e) => {
+//     mouseX = e.clientX;
+//     mouseY = e.clientY;
+// });
+
+// window.addEventListener('mouseleave', () => {
+//     mouseX = null;
+//     mouseY = null;
+// });
+
+// init();
+// animate();
